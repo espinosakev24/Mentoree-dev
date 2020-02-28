1745143755683b33206e0a4fded2b87ba801c00a
mrdoomus <mrdoom.official@gmail.com>
2020-02-27 23:40:14 -0500

    testing: [Update] database connection is now implemented!

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+//We only need Router method from express
 const router = express.Router();
 
+//At home we send "Hello World" on screen as a response to request
 router.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+//exporting router so it can be used by main index.js
 module.exports = router;
