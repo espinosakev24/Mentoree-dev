8c9e4afcb01b550b6dba1db97e661e11edb21f69
mrdoomus <mrdoom.official@gmail.com>
2020-02-28 18:05:19 -0500

    testing: [Update] to Index.js just handling sv startup [Upload] App.js as route handler and routes/ files with REST methods

--- database/forkeys_setup.sql
+++ database/forkeys_setup.sql
@@ -1,11 +1,11 @@
 /*Sets Foreign Keys on Tables*/
 USE mentoree_db
 ALTER TABLE students
-ADD FOREIGN KEY (post_id) REFERENCES posts(post_id);
+ADD FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE;
 
 ALTER TABLE posts
-ADD FOREIGN KEY (student_id) REFERENCES students(student_id),
-ADD FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id);
+ADD FOREIGN KEY (student_id) REFERENCES students(student_id) ON DELETE CASCADE,
+ADD FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id) ON DELETE CASCADE;
 
 ALTER TABLE teachers
-ADD FOREIGN KEY (post_id) REFERENCES posts(post_id);
+ADD FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE;
--- src/index.js
+++ src/index.js
@@ -1,56 +1,9 @@
-const express = require('express');
-const morgan = require('morgan');
+const app = require('./app');
+require('./database');
 
-const pool = require('./database')
-
-/*
-INITS
-Initializate modules
-*/
-const app = express();
-
-
-/*
-SETTINGS
-Configuration for the server
-*/
-//Setting port on 4000 or the environment already defined port
-app.set('port', process.env.PORT || 4000);
-
-
-/*
-MIDDLEWARES
-Functions executed for every request, whereas client or thir party apps
-*/
-//Logs messages with 'dev' format thanks to Morgan
-app.use(morgan('dev'));
-//when extended of urlencoded is false, we can only allow text, not images.
-app.use(express.urlencoded({ extended: false }));
-//To receive and send json
-app.use(express.json());
-
-
-/*
-GLOBAL VARIABLES
-Variables for a global usage in the APP
-*/
-//This function is like a stop, gets a req, does something and send a res, and then next() To continue the APP
-app.use((req, res, next) => {
-    next();
-});
-
-/*
-ROUTES
-How endpoints should behave
-*/
-//App uses what index.js has
-app.use(require('./routes/index.js'));
-
-
-/*
-SV START
-Defining on which port and what behavior
-*/
-app.listen(app.get('port'), () => {
+async function main() {
+    await app.listen(app.get('port'));
     console.log('Server on port', app.get('port'));
-});
+}
+
+main();
