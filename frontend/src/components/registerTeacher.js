32f4bcfda6c186dff48dcc67f987fdd3284e20a2
espinosakev24 <916@holbertonschool.com>
2020-03-09 11:07:27 -0500

    testing: [UPDATES] login and register teacher

--- frontend/src/components/registerTeacher.js
+++ frontend/src/components/registerTeacher.js
@@ -53,138 +53,114 @@ export default class RegisterTeacher extends Component {
 
   render () {
     return (
-      <div class='container p-4'>
-        <Header />
-        <div class='row'>
-          <div class='col-md-4 mx-auto'>
-            <div class='cad text-center'>
-              <div class='card-header'>
-                <h3>Register</h3>
-              </div>
-              <div class='card-body'>
-                <form noValidate onSubmit={this.onSubmit}>
-                  <div className='form-group'>
-                    <label htmlFor='first_name'>First Name</label>
-                    <input
-                      type='text'
-                      className='form-control'
-                      name='first_name'
-                      placeholder='Enter first name'
-                      value={this.state.first_name}
-                      onChange={this.onChange}
-                    />
-                  </div>
+      <div class="container pl-10 pr-10 mt-5">
+                <Header />
+                <h3><strong>Register</strong></h3> <br/>
+                <form noValidate onSubmit={this.onSubmit} className="" id="main-form">
+                    <div className="row d-flex justify-content-between ">
+                        <div className="form-group col-6">
+                            <label htmlFor="first_name" id="text">First Name</label>
+                            <input type="text"
+                            className="form-control"
+                            name="first_name"
+                            placeholder="Enter first name"
+                            value={this.state.first_name}
+                            onChange={this.onChange}
+                            id="fields"
+                            />
+                        </div>
+                        <div className="form-group col-6">
+                            <label htmlFor="last_name" id="text">Last Name</label>
+                            <input type="text"
+                            className="form-control"
+                            name="last_name"
+                            placeholder="Enter Last name"
+                            value={this.state.last_name}
+                            onChange={this.onChange}
+                            id="fields"
+                            />
+                        </div>
+                    </div> <br/>
 
-                  <div className='form-group'>
-                    <label htmlFor='last_name'>Last Name</label>
-                    <input
-                      type='text'
-                      className='form-control'
-                      name='last_name'
-                      placeholder='Enter Last name'
-                      value={this.state.last_name}
-                      onChange={this.onChange}
-                    />
-                  </div>
+                <div className="row d-flex justify-content-between ">
+                    <div className="form-group col-6">
+                        <label htmlFor="email" id="text">Email</label>
+                        <input type="email"
+                        className="form-control"
+                        name="email"
+                        placeholder="Enter Email"
+                        value={this.state.email}
+                        onChange={this.onChange}
+                        id="fields"
+                        />
+                    </div>
 
-                  <div className='form-group'>
-                    <label htmlFor='contact'>Contact</label>
-                    <input
-                      type='text'
-                      className='form-control'
-                      name='contact'
-                      placeholder='Enter Contact'
-                      value={this.state.contact}
-                      onChange={this.onChange}
-                    />
-                  </div>
+                    <div className="form-group col-6">
+                        <label htmlFor="password" id="text">Password</label>
+                        <input type="password"
+                        className="form-control"
+                        name="password"
+                        placeholder="Enter Password"
+                        value={this.state.password}
+                        onChange={this.onChange}
+                        id="fields"
+                        />
+                    </div>
+                </div> <br/>
+                <div className="row d-flex justify-content-between">
+                    <div className="form-group col-6">
+                        <label htmlFor="contact" id="text">Contact</label>
+                        <input type="text"
+                        className="form-control"
+                        name="contact"
+                        placeholder="Enter Contact"
+                        value={this.state.contact}
+                        onChange={this.onChange}
+                        id="fields"
+                        />
+                    </div>
 
-                  <div className='form-group'>
-                    <label htmlFor='password'>Password</label>
-                    <input
-                      type='password'
-                      className='form-control'
-                      name='password'
-                      placeholder='Enter Password'
-                      value={this.state.password}
-                      onChange={this.onChange}
-                    />
-                  </div>
+                    <div className="form-group col-6">
+                        <label htmlFor="education" id="text">Education</label>
+                        <input type="text"
+                        className="form-control"
+                        name="education"
+                        placeholder="Enter Education"
+                        value={this.state.education}
+                        onChange={this.onChange}
+                        id="fields"
+                        />
+                    </div>
+                </div> <br/>
 
-                  <div className='form-group'>
-                    <label htmlFor='location'>Location</label>
-                    <input
-                      type='text'
-                      className='form-control'
-                      name='location'
-                      placeholder='Enter Location'
-                      value={this.state.location}
-                      onChange={this.onChange}
-                    />
-                  </div>
+                <div className="row d-flex justify-content-between">
+                    <div className="form-group col-6">
+                        <label htmlFor="location" id="text">Location</label>
+                        <input type="text"
+                        className="form-control"
+                        name="location"
+                        placeholder="Enter Location"
+                        value={this.state.location}
+                        onChange={this.onChange}
+                        id="fields"
+                        />
+                    </div>
 
-                  <div className='form-group'>
-                    <label htmlFor='age'>Age</label>
-                    <input
-                      type='number'
-                      className='form-control'
-                      name='age'
-                      placeholder='Enter Age'
-                      value={this.state.age}
-                      onChange={this.onChange}
-                    />
-                  </div>
-
-                  <div className='form-group'>
-                    <label htmlFor='email'>Email</label>
-                    <input
-                      type='email'
-                      className='form-control'
-                      name='email'
-                      placeholder='Enter Email'
-                      value={this.state.email}
-                      onChange={this.onChange}
-                    />
-                  </div>
-
-                  <div className='form-group'>
-                    <label htmlFor='education'>Education</label>
-                    <input
-                      type='text'
-                      className='form-control'
-                      name='education'
-                      placeholder='Enter Education'
-                      value={this.state.education}
-                      onChange={this.onChange}
-                    />
-                  </div>
-
-                  <div className='form-group'>
-                    <label htmlFor='biography'>Biography</label>
-                    <input
-                      type='text'
-                      className='form-control'
-                      name='biography'
-                      placeholder='Enter Biography'
-                      value={this.state.biography}
-                      onChange={this.onChange}
-                    />
-                  </div>
-
-                  <div className='form-group'>
-                    <label htmlFor='fields'>Fields</label>
-                    <input
-                      type='text'
-                      className='form-control'
-                      name='fields'
-                      placeholder='Enter Fields'
-                      value={this.state.fields}
-                      onChange={this.onChange}
-                    />
-                  </div>
-
-                  <div className='form-group'>
-                    <label htmlFor='methodology'>Methodology</label>
+                    <div className="form-group col-6">
+                        <label htmlFor="age" id="text">Age</label>
+                        <input type="number"
+                        className="form-control"
+                        name="age"
+                        placeholder="Enter Age"
+                        value={this.state.age}
+                        onChange={this.onChange}
+                        id="fields"
+                        />
+                    </div>
+                </div> <br/>
+                <div className="row d-flex justify-content-between">
+                    <div className='form-group col-6'>
+                    <label htmlFor='methodology' id="text">Methodology</label>
                     <input
                       type='text'
                       className='form-control'
@@ -192,17 +168,49 @@ export default class RegisterTeacher extends Component {
                       placeholder='Enter Methodology'
                       value={this.state.methodology}
                       onChange={this.onChange}
+                      id="fields"
                     />
-                  </div>
-                  <button type='submit' class='btn btn-success btn-block'>
-                                        Register
-                  </button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+                    </div>
+
+                    <div className='form-group col-6'>
+                    <label htmlFor='fields' id="text">Fields</label>
+                    <input
+                      type='text'
+                      className='form-control'
+                      name='fields'
+                      placeholder='Enter Fields'
+                      value={this.state.fields}
+                      onChange={this.onChange}
+                      id="fields"
+                    />
+                    </div>
+                </div> <br/>
+
+                <div className="row d-flex justify-content-between">
+                    <div className='form-group col-12'>
+                    <label htmlFor='biography' id="text">Biography</label>
+                    <input
+                      type='text'
+                      className='form-control'
+                      name='biography'
+                      placeholder='Enter Biography'
+                      value={this.state.biography}
+                      onChange={this.onChange}
+                      id="fields"
+                    />
+                    </div>
+                </div>
+                <br/>
+                <div className="row col-12 d-flex justify-content-end">
+                    <button type="submit" class="btn btn-1">
+                        Cancel
+                    </button>
+                    <button type="submit" class="btn btn-2">
+                        Register
+                    </button>
+                </div>
+                    </form> <br/> <br/>
+                </div>
     );
   }
 }
